33810e2a8e5f3f041c2352e17fd4df2e2626c512
riceknot <118870849+riceknot@users.noreply.github.com>
2023-08-31 16:24:06 +0700

    Update App.js

--- Full_Stack_Assignment_2 - Copy/client/src/App.js
+++ Full_Stack_Assignment_2 - Copy/client/src/App.js
@@ -13,26 +13,27 @@ import EditCustomer from './components/editCustomer.component';
 import productPage from './components/productPage';
 import productCreate from './components/createProduct';
 
+import Register from './components/Register';
+
 function App() {
   return (
     <Router>
       <div className='container'>
         <NavBar />
-        <br/>
+        <br />
         <Routes>
-          <Route path='/' Component={welcomPage}/>
-          <Route path='/customer' Component={customerList}/>
-          <Route path='/sellers' Component={sellerList}/>
-          <Route path='/customer/edit/:id' Component={EditCustomer}/>
-          <Route path='/seller/edit/:id' Component={EditSeller}/>
-          <Route path='/customer/create' Component={CreateCustomer}/>
-          <Route path='/sellers/create' Component={CreateSeller}/>
-          <Route path='/product' Component={productPage}/>
-          <Route path='/product/create' Component={productCreate}/>
+          <Route path='/' Component={welcomPage} />
+          <Route path='/customer' Component={customerList} />
+          <Route path='/sellers' Component={sellerList} />
+          <Route path='/customer/edit/:id' Component={EditCustomer} />
+          <Route path='/seller/edit/:id' Component={EditSeller} />
+          <Route path='/customer/create' Component={CreateCustomer} />
+          <Route path='/sellers/create' Component={CreateSeller} />
+          <Route path='/product' Component={productPage} />
+          <Route path='/product/create' Component={productCreate} />
+          <Route path='/register' Component={Register} />
         </Routes>
       </div>
     </Router>
-  );  
+  );
 }
-
-export default App;
