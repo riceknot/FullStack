46326b662bcb3d2bbff7b603c70576d57551b3f1
riceknot <118870849+riceknot@users.noreply.github.com>
2023-08-31 16:24:37 +0700

    Add files via upload

---
+++ Full_Stack_Assignment_2 - Copy/client/src/components/Register.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+
+export default function Register() {
+    const [role, setRole] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+    const [address, setAddress] = useState("");
+    const [businessName, setBusinessName] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        Axios.post('http://localhost:3000/register', {
+            role: role,
+            email: email,
+            phone: phone,
+            password: password,
+            address: address,
+            businessName: businessName
+        })
+            .then(() => {
+                console.log('Account successfully registered!');
+            })
+            .catch((error) => {
+                console.log(error.message);
+            })
+    }
+
+    return (
+        <div className="container">
+            <div className="Register-form">
+                <form onSubmit={handleSubmit}>
+                    <label>Role</label>
+                    <select className="role" onChange={(e) => { setRole(e.target.value) }}>
+                        <option value="">Choose a role</option>
+                        <option value="seller">Seller</option>
+                        <option value="customer">Customer</option>
+                    </select>
+
+                    <label>Email</label>
+                    <input
+                        className="email"
+                        type="text"
+                        onChange={(e) => { setEmail(e.target.value) }}
+                    />
+
+                    <label>Phone</label>
+                    <input
+                        className="phone"
+                        type="number"
+                        onChange={(e) => { setPhone(e.target.value) }}
+                    />
+
+                    <label>Password</label>
+                    <input
+                        className="password"
+                        type="text"
+                        onChange={(e) => { setPassword(e.target.value) }}
+                    />
+
+                    {
+                        // If the user choose 'customer', shows Address input and hide Business Name.
+                    }
+                    {role === 'customer' && (
+                        <div>
+                            <label>Address</label>
+                            <input
+                                className="address"
+                                type="text"
+                                onChange={(e) => { setAddress(e.target.value) }}
+                            />
+                        </div>
+                    )}
+
+                    {
+                        // If the user choose 'customer', shows Business Name input and hide Address.
+                    }
+                    {role === 'seller' && (
+                        <div>
+                            <label>Business Name</label>
+                            <input
+                                className="businessName"
+                                type="text"
+                                onChange={(e) => { setBusinessName(e.target.value) }}
+                            />
+                        </div>
+                    )}
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    );
+}
