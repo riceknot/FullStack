37d2035240f33da1fd5cd04d1b997879c42bf6fc
riceknot <118870849+riceknot@users.noreply.github.com>
2023-09-08 18:28:35 +0700

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 let Category = require('../models/category.model');
+let Product = require('../models/product.model');
 
 router.route('/')
     .get((req, res) => {  //Send information of all sellers to Admin page.
@@ -45,33 +46,37 @@ router.route('/category/add')
                 })
                 .catch((error) => {
                     console.log(error.message)
-                    return res.send('Error with adding main category.')
                 });
         } else if (cateType === 'sub') {
             Category.findOne({ id: req.body.parentID, categoryType: 'main' })
                 .then((subCate) => {
-                    const newCate = new Category({
-                        categoryName: req.body.categoryName,
-                        categoryType: req.body.categoryType,
-                        parentID: req.body.parentID
-                    });
-                    newCate.save()
-                        .then(() => {
-                            console.log('New sub category added!');
-                            return res.send('New sub category added!');
-                        })
-                        .catch((error) => {
-                            console.log(error.message)
-                            return res.send('Error with adding sub category.')
+                    if (subCate.length === 0) {
+                        return console.log('Cannot find Main category ID.')
+                    } else {
+                        const newCate = new Category({
+                            categoryName: req.body.categoryName,
+                            categoryType: req.body.categoryType,
+                            parentID: req.body.parentID
                         });
+                        newCate.save()
+                            .then(() => {
+                                console.log('New sub category added!');
+                                return res.send('New sub category added!');
+                            })
+                            .catch((error) => {
+                                console.log(error.message)
+                            })
+                    };
                 })
                 .catch((error) => {
                     console.log(error.message)
-                    return res.send('Error with adding sub category.')
                 });
         } else {
             Category.findOne({ id: req.body.parentID, categoryType: 'sub' })
                 .then((ssubCate) => {
+                    if (ssubCate.length === 0) {
+                        return console.log('Cannot find Sub category ID.')
+                    }
                     const newCate = new Category({
                         categoryName: req.body.categoryName,
                         categoryType: req.body.categoryType,
@@ -93,5 +98,60 @@ router.route('/category/add')
                 });
         }
     })
+
+
+router.route('/category/delete-main')
+    .post((req, res) => {
+        const SubID = req.body.id;
+        Category.find({ categoryType: 'sub', parentID: SubID })
+            .then((foundCate) => {
+                if (foundCate.length === 0) {
+                    Category.findByIdAndDelete(SubID)
+                        .then((deleted) => {
+                            console.log('Main catergory deleted!');
+                        })
+                        .catch((error) => console.log(error.message))
+                } else {
+                    console.log('Please delete all associated sub categories.')
+                }
+            })
+            .catch((error) => console.log(error.message))
+    });
+
+router.route('/category/delete-sub')
+    .post((req, res) => {
+        const SubID = req.body.id;
+        Category.find({ categoryType: 'ssub', parentID: SubID })
+            .then((foundCate) => {
+                if (foundCate.length === 0) {
+                    Category.findByIdAndDelete(SubID)
+                        .then((deleted) => {
+                            console.log('Sub category deleted!');
+                        })
+                        .catch((error) => console.log(error.message))
+                } else {
+                    console.log('Please delete all associated sub-sub categories first.');
+                }
+            })
+            .catch((error) => console.log(error.message))
+    });
+
+router.route('/category/delete-ssub')
+    .post((req, res) => {
+        Product.find({ category: req.body.categoryName })
+            .then((foundPro) => {
+                if (foundPro.length === 0) {
+                    Category.findOneAndDelete({ categoryName: req.body.categoryName })
+                        .then((deleted) => {
+                            console.log('Sub-sub category deleted!');
+                        })
+                        .catch((error) => console.log(error.message))
+                } else {
+                    console.log('Please delete all associated products.')
+                }
+            })
+            .catch((error) => console.log(error.message))
+    })
+
 
 module.exports = router;
