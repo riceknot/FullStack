b7f44478cb5be48ce40b037178706afdc26cceec
s3759316 <81907684+s3759316@users.noreply.github.com>
2023-09-08 23:09:10 +0700

    Delete backend directory

--- backend/routes/order.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-let Order = require('../models/order.model');
-
-router.route('/').get((req,res) => {
-    Order.find()
-        .then(order => res.json(order))
-        .catch(err => res.status(400).json('Error: ' + err))
-});
